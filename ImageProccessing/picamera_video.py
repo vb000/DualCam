504f8ee943d7f3b3f764ac8fad14ccc61ac12f49
Bandhav Veluri <bandhav.veluri00@gmail.com>
2022-03-22 19:13:46 -0700

    Updated picam video script

--- ImageProccessing/picamera_video.py
+++ ImageProccessing/picamera_video.py
@@ -16,28 +16,41 @@ parser.add_argument('--record_time', default=10, type=int,
                     help="Recording time in seconds.")
 parser.add_argument('--output_dir', default='out',
                     help="Output directory with RGB frames.")
+parser.add_argument('--stats', default=0, type=int,
+                    help="Enable stats.")
 
 class Output(object):
-    def __init__(self, out_dir=None):
+    def __init__(self, out_dir=None, stats=0):
         self.out_dir = out_dir
         if not os.path.exists(self.out_dir):
             os.makedirs(self.out_dir)
-        self.frame_count = 0
-        self.total_write_time = 0
+        self.stats = stats
+        if self.stats != 0:
+            self.prev_t_stamp = None
+            self.frame_count = 0
+            self.total_write_time = 0
+            self.total_time_period = 0
 
     def write(self, s):
         t_stamp = time.time_ns() // 1000
         filename = os.path.join(self.out_dir, '%d.jpg' % t_stamp)
         with open(filename, 'wb') as f:
             f.write(s)
-        self.frame_count += 1
-        t_stamp_end = time.time_ns() // 1000
-        self.total_write_time += t_stamp_end - t_stamp
+        if self.stats != 0:
+            t_stamp_end = time.time_ns() // 1000
+            if self.frame_count != 0:
+                self.total_time_period += t_stamp - self.prev_t_stamp
+                self.total_write_time += t_stamp_end - t_stamp
+            self.prev_t_stamp = t_stamp
+            self.frame_count += 1
 
     def flush(self):
-        print('%d frames are written' % self.frame_count)
-        print("Avg. write time per frame: %d us" %
-              self.total_write_time / self.frame_count)
+        if self.stats != 0:
+            print('%d frames are written' % self.frame_count)
+            print("Avg. time period between frames = %d us" %
+                  (self.total_time_period // (self.frame_count - 1)))
+            print("Avg. write time per frame = %d us" %
+                  (self.total_write_time // (self.frame_count - 1)))
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -46,7 +59,8 @@ if __name__ == "__main__":
     with picamera.PiCamera() as camera:
         camera.resolution = (args.width, args.height)
         camera.framerate = args.frame_rate
-        camera.start_recording(Output(args.output_dir), format='mjpeg',
+        camera.start_recording(Output(args.output_dir, args.stats),
+                               format='mjpeg',
                                quality=100)
         camera.wait_recording(args.record_time)
         camera.stop_recording()
