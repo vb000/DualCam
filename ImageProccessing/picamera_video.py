53f3991e698ac43ad4b03f6f79ffe9fad5f5e5a2
Bandhav Veluri <bandhav.veluri00@gmail.com>
2022-03-22 17:55:01 -0700

    Updated picam video script

--- ImageProccessing/picamera_video.py
+++ ImageProccessing/picamera_video.py
@@ -1,27 +1,49 @@
+import argparse
+import os
+import tracemalloc
 import picamera
-import numpy as np
-from PIL import Image
+
+parser = argparse.ArgumentParser(
+    description="Records a video from PiCamera.")
+parser.add_argument('--frame_rate', default=15, type=int,
+                    help="Input frame rate in FPS.")
+parser.add_argument('--width', default=640, type=int,
+                    help="Frame width.")
+parser.add_argument('--height', default=480, type=int,
+                    help="Frame height.")
+parser.add_argument('--record_time', default=10, type=int,
+                    help="Recording time in seconds.")
+parser.add_argument('--output_dir', default='out',
+                    help="Output directory with RGB frames.")
 
 class Output(object):
-    def __init__(self, resolution=(640, 480)):
-        self.frames = 0
-        self.resolution = resolution
-        self.images = []
+    def __init__(self, out_dir=None):
+        self.frames = []
+        self.out_dir = out_dir
+        if not os.path.exists(self.out_dir):
+            os.makedirs(self.out_dir)
 
     def write(self, s):
-        frame = np.frombuffer(s, dtype=np.uint8)
-        frame.shape = (self.resolution[1], self.resolution[0], 3)
-        self.images.append(Image.fromarray(frame, 'RGB'))
-        self.frames += 1
+        self.frames.append(s)
 
     def flush(self):
-        #for i, img in enumerate(self.images):
-        #    img.save('frame%d.png' % i)
-        print('%d frames are written' % self.frames)
+        print('%d frames are written' % len(self.frames))
+        for i, frm in enumerate(self.frames):
+            filename = os.path.join(self.out_dir, 'frame%d.jpg' % i)
+            with open(filename, 'wb') as f:
+                f.write(self.frames[i])
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.framerate = 15
-    camera.start_recording(Output(camera.resolution), format='rgb')
-    camera.wait_recording(100)
-    camera.stop_recording()
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    tracemalloc.start()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (args.width, args.height)
+        camera.framerate = args.frame_rate
+        camera.start_recording(Output(args.output_dir), format='mjpeg',
+                               quality=100)
+        camera.wait_recording(args.record_time)
+        camera.stop_recording()
+    print("Peak memory usage: %d MB" %
+          (tracemalloc.get_traced_memory()[1] // 10**6))
+    tracemalloc.stop()
