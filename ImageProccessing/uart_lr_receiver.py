48710d7231d72fc29c189ec2a562e6232a218da0
Bandhav Veluri <bandhav.veluri00@gmail.com>
2022-04-01 16:04:47 -0700

    Updated device addresses

--- ImageProccessing/uart_lr_receiver.py
+++ ImageProccessing/uart_lr_receiver.py
@@ -7,7 +7,7 @@ from uart_hr_receiver import uart_read_video
 
 parser = argparse.ArgumentParser(
     description="Reads data from uart and writes PNG output images.")
-parser.add_argument('--input_device', default='/dev/tty.usbmodem142103',
+parser.add_argument('--input_device', default='/dev/tty.usbmodem1421103',
                     help="Input directory with RAW pixel data.")
 parser.add_argument('--input_rate', default=6.0e6,
                     help="Input baud rate.")
