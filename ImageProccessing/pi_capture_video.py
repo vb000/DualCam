363c12ecf40c17497061cc1dc0838ee4b9b34fd2
Bandhav Veluri <bandhav.veluri00@gmail.com>
2022-04-05 01:30:18 -0700

    Updated pi capture

--- ImageProccessing/pi_capture_video.py
+++ ImageProccessing/pi_capture_video.py
@@ -15,7 +15,7 @@ parser.add_argument('--height', default=480, type=int,
                     help="Frame height.")
 parser.add_argument('--record_time', default=10, type=int,
                     help="Recording time in seconds.")
-parser.add_argument('--output_dir', default='out',
+parser.add_argument('--output_dir', default='data',
                     help="Output directory with RGB frames.")
 parser.add_argument('--stats', default=0, type=int,
                     help="Enable stats.")
@@ -31,31 +31,47 @@ class Output(object):
         self.led1.on()
         self.led2.on()
         self.origin_time = time.time_ns() // 1000
+        self.frame_count = 0
+        self.prev_t_stamp = self.origin_time
         if self.stats != 0:
-            self.prev_t_stamp = None
-            self.frame_count = 0
+            self.max_time_period = 0
+            self.max_write_time = 0
             self.total_write_time = 0
             self.total_time_period = 0
 
     def write(self, s):
         t_stamp = time.time_ns() // 1000
+        time_period = t_stamp - self.prev_t_stamp
+        if self.frame_count > 1 and time_period > 100000:
+            print("Frame rate error at frame %d: time period = %d" % (
+                self.frame_count, time_period))
         t_delta_ms = (t_stamp - self.origin_time) // 1000
-        filename = os.path.join(self.out_dir, '%d.jpg' % t_delta_ms)
+        filename = os.path.join(
+            self.out_dir,
+            '%09d_%09d.jpg' % (self.frame_count,
+                               t_delta_ms))
         with open(filename, 'wb') as f:
             f.write(s)
         if self.stats != 0:
             t_stamp_end = time.time_ns() // 1000
+            write_time = t_stamp_end - t_stamp
             if self.frame_count != 0:
-                self.total_time_period += t_stamp - self.prev_t_stamp
-                self.total_write_time += t_stamp_end - t_stamp
-            self.prev_t_stamp = t_stamp
-            self.frame_count += 1
+                self.total_time_period += time_period
+                self.total_write_time += write_time
+                if self.max_time_period < time_period:
+                    self.max_time_period = time_period
+                if self.max_write_time < write_time:
+                    self.max_write_time = write_time
+        self.prev_t_stamp = t_stamp
+        self.frame_count += 1
 
     def flush(self):
         self.led1.off()
         self.led2.off()
         if self.stats != 0:
-            print('%d frames are written' % self.frame_count)
+            print("%d frames are written" % self.frame_count)
+            print("Max time period = %d us" % self.max_time_period)
+            print("Max write time = %d us" % self.max_write_time)
             print("Avg. time period between frames = %d us" %
                   (self.total_time_period // (self.frame_count - 1)))
             print("Avg. write time per frame = %d us" %
